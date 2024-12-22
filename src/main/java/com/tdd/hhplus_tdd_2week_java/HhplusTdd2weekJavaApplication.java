30ff8ffb13f8dbf24f46d4ab46db904308e843aa
GustavEiffels <sibuk9315@gmail.com>
2024-12-23 00:26:48 +0900

    Initial commit

---
+++ src/main/java/com/tdd/hhplus_tdd_2week_java/HhplusTdd2weekJavaApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.hhplus_tdd_2week_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusTdd2weekJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusTdd2weekJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdd/hhplus_tdd_2week_java/HhplusTdd2weekJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.hhplus_tdd_2week_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusTdd2weekJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
