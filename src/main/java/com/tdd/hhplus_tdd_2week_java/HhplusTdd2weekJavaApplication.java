7404e49ed5c125eb6c93f575c6a2aad2c0f41af7
GustavEiffels <sibuk9315@gmail.com>
2024-12-25 21:21:05 +0900

    [reset] pr 실수로 master 브랜치 초기화

--- src/main/java/com/tdd/hhplus_tdd_2week_java/HhplusTdd2weekJavaApplication.java
+++ src/main/java/com/tdd/hhplus_tdd_2week_java/HhplusTdd2weekJavaApplication.java
@@ -2,9 +2,7 @@ package com.tdd.hhplus_tdd_2week_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class HhplusTdd2weekJavaApplication {
 
